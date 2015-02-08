b4d10d0ede695c704e99eac28210751a419b2514
ryan <ryan@Abigails-MacBook-Air.local>
2015-02-07 22:03:09 -0800

    First Commit ++ OS X menu bar application ++ Stores current (24hr) time to the menu bar --- much to be desired

--- ServerTime/AppDelegate.swift
+++ ServerTime/AppDelegate.swift
@@ -11,9 +11,34 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+//    @IBOutlet weak var window: NSWindow!
 
-
+    var statusBar = NSStatusBar.systemStatusBar()
+    var statusBarItem : NSStatusItem = NSStatusItem()
+    var menu: NSMenu = NSMenu()
+    
+    func timeString() -> String {
+        var date = NSDate()
+        var calendar = NSCalendar.currentCalendar()
+        var components = calendar.components(.CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitSecond, fromDate: date)
+        var hour = components.hour
+        var minutes = components.minute
+        var seconds = components.second
+        return "\(hour):\(minutes):\(seconds)"
+    }
+   
+    override func awakeFromNib() {
+        // Add statusBarItem
+        statusBarItem = statusBar.statusItemWithLength(-1)
+        statusBarItem.menu = menu
+        statusBarItem.title = "Local"
+        
+        // Current Time
+        let tyme = timeString()
+        var displayTyme = String(tyme)
+        statusBarItem.title = displayTyme
+    }
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
     }
